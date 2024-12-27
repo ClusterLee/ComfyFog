0088ad3fc5dab135362be76c72639435dd594068
clusterlee <clusterlee@qq.com>
2024-12-27 16:26:12 +0800

    fog_model add extra checkpoint dir ok

--- fog_model.py
+++ fog_model.py
@@ -12,7 +12,7 @@ class FogModel:
     def get_folder_paths_info(self):
 
         try:
-            logger.debug( f"-------------folder_paths info-------------\n: {folder_paths.folder_names_and_paths}")
+            logger.debug( f"-------------folder_paths info-------------:\n {folder_paths.folder_names_and_paths}")
         except Exception as e:
             logger.error(f"Failed to get get_folder_paths: {e}")        
 
