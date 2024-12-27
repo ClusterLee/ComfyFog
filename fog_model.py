2d9fcf7d03b733388bb841f5a23bdbc9d4f1a14c
clusterlee <clusterlee@qq.com>
2024-12-27 16:29:09 +0800

    fog_model add extra checkpoint

--- fog_model.py
+++ fog_model.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import folder_paths
 
@@ -6,13 +7,26 @@ logger = logging.getLogger('ComfyFog')
 
 class FogModel:
     def __init__(self):
+  
+        # 添加ComfyFog 额外模型目录，通常为远程目录，本地comfyui模型目录优先
+
+        try:
+            fog_model_dir = os.path.join(os.path.dirname(__file__), "models")
+            fog_mode_checkpoints = os.path.join(fog_model_dir, "checkpoints")
+            folder_paths.add_model_folder_path("checkpoints", fog_mode_checkpoints)
+            
+            if not os.path.exists(fog_mode_checkpoints):
+                os.makedirs(fog_mode_checkpoints)
+
+        except Exception as e:
+            logger.error(f"Failed to init FogModel: {e}")        
 
         return
         
     def get_folder_paths_info(self):
 
         try:
-            logger.debug( f"-------------folder_paths info-------------:\n {folder_paths.folder_names_and_paths}")
+            logger.debug( f"【folder_paths info】\n {folder_paths.folder_names_and_paths}")
         except Exception as e:
             logger.error(f"Failed to get get_folder_paths: {e}")        
 
